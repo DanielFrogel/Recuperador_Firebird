4f8ba5c04f3ffbbb4b124c56b777e3c90a2c0d6e
DanielFrogel <164185757+DanielFrogel@users.noreply.github.com>
2024-04-13 13:33:47 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from modelos.janelas import janela_principal
+
+"""
+    Arquivo principal inicializa a janela
+
+"""
+
+
+def main():
+    janela_principal()
+
+if __name__ == '__main__':
+    main()
